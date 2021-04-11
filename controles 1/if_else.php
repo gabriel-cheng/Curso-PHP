0677e92e2b901667ee94923e026529ed82b1314c
Gabriel Carvalho <gabriel_carvalho.contato@outlook.com>
2021-04-11 02:58:32 -0300

    Conclusão da aula de If/Else parte 2

--- controles 1/if_else.php
+++ controles 1/if_else.php
@@ -22,3 +22,36 @@
     { /*Colocar charves significa abrir um bloco de código*/
         echo 'Serei impresso?';
     }
+    if(true)
+    {
+        echo '<br>É verdadeiro! - Parte A';
+        echo '<br>É verdadeiro! - Parte B<br>';
+    }
+    else //Todo else precisa estar associado à um if
+       {
+        echo '<br>É falso!';
+        echo '<br>É falso!';
+       }
+
+
+       if(false)
+       {
+            echo '<br>Passo A<br>';
+       }
+       else if(true)
+       {
+           echo 'Passo B<br>';
+       }
+       else if(true)
+       {
+           echo 'Passo C<br>';
+       }
+       else if(true)
+       {
+           echo 'Passo D<br>';
+       }
+       else if(true)
+       {
+           echo 'Passo E<br>';
+       }
+?>
