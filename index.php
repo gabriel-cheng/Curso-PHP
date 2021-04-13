06ba093980e25cfbc937102b0c96421c9d1e31b6
Gabriel Carvalho <gabriel_carvalho.contato@outlook.com>
2021-04-13 12:12:04 -0300

    Adição do título MÓDULOS com border_bottom

--- index.php
+++ index.php
@@ -16,6 +16,7 @@
     </header>
     <main class="principal">
         <div class="conteudo">
+            <h1 class="module_title">MÓDULOS</h1>
             <nav class="modulos">
                 <div class="modulo roxo">
                     <h3>4. Controles I</h3>
