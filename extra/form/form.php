3664e1ccce49eaaea87357be11f56378b8f760f2
Gabriel Carvalho <gabriel_carvalho.contato@outlook.com>
2021-04-14 13:52:29 -0300

    Arquivo form.php deletado

--- extra/form/form.php
+++
@@ -1,7 +0,0 @@
-<div class="titulo">Exemplo de form com Login e Senha</div>
-
-
-<form action="" method="GET">
-    <input type="text" name="login" placeholder="Login">
-    <input type="password" name="senha" placeholder="Senha">
-</form>
